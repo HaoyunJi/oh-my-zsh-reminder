c06ac53ba9c5ea89ebd79780f5dc9d6a6618d932
HaoyunJi <57861564+HaoyunJi@users.noreply.github.com>
2023-07-14 20:02:35 +0800

    Update reminder.plugin.zsh

--- reminder.plugin.zsh
+++ reminder.plugin.zsh
@@ -36,6 +36,7 @@ function todo_task_done {
     todo_tasks[index]=()
     todo_tasks_colors[index]=()
     todo_save
+    todo_display
 }
 
 function todo_display {
@@ -52,7 +53,6 @@ function _todo_task_done {
     load_tasks
     if [[ ${#todo_tasks} -gt 0 ]] then
       compadd $(echo ${TODO_TASKS} | tr ':' '\n')
-      todo_display
     fi
   }
 
